8e5abbce53046301a1af7dd6243426864632de83
Goncalo-Chambel <goncalorrc@gmail.com>
2024-04-07 18:48:48 +0100

    Refactoring cleaning.py

--- life_expectancy/cleaning.py
+++ life_expectancy/cleaning.py
@@ -15,18 +15,25 @@ def to_float(value):
             return None
 
 
-def clean_data(country="PT"):
+def load_data() -> pd.DataFrame:
+    """Function to load data from file"""
+    data = pd.read_csv("life_expectancy/data/eu_life_expectancy_raw.tsv", sep="\t")
+    return data
+
+def save_data(data: pd.DataFrame) -> None:
+    """Function to save data to file"""
+    data.to_csv("life_expectancy/data/pt_life_expectancy.csv", index=False)
+
+def clean_data(data: pd.DataFrame, country:str="PT") -> pd.DataFrame:
     """Function used to clean raw data"""
 
-    data_raw = pd.read_csv("life_expectancy/data/eu_life_expectancy_raw.tsv", sep="\t")
-
-    composite_columns = data_raw.columns[0].split(",")
+    composite_columns = data.columns[0].split(",")
     composite_columns[-1] = composite_columns[-1].split("\\")[0]
 
-    split_columns = data_raw.iloc[:, 0].str.split(",", expand=True)
+    split_columns = data.iloc[:, 0].str.split(",", expand=True)
     split_columns.columns = composite_columns
 
-    df_transformed = pd.concat([split_columns, data_raw.iloc[:, 1:]], axis=1)
+    df_transformed = pd.concat([split_columns, data.iloc[:, 1:]], axis=1)
 
     df = pd.melt(df_transformed, id_vars=composite_columns, var_name="year", value_name="value")
     df["year"] = df["year"].astype(int)
@@ -37,12 +44,14 @@ def clean_data(country="PT"):
     df.dropna(inplace=True)
 
     df = df[df["region"] == country]
-    df.to_csv("life_expectancy/data/pt_life_expectancy.csv", index=False)
-
+    return df
 
 if __name__ == "__main__":  # pragma: no cover
 
     parser = argparse.ArgumentParser()
     parser.add_argument("country")
     args = parser.parse_args()
-    clean_data(args.country)
+
+    data_raw = load_data()
+    data_cleaned = clean_data(data_raw, args.country)
+    save_data(data_cleaned)
--- life_expectancy/tests/test_cleaning.py
+++ life_expectancy/tests/test_cleaning.py
@@ -2,13 +2,16 @@
 
 import pandas as pd
 
-from life_expectancy.cleaning import clean_data
+from life_expectancy.cleaning import clean_data, load_data, save_data
 from . import OUTPUT_DIR
 
 
 def test_clean_data(pt_life_expectancy_expected):
     """Run the `clean_data` function and compare the output to the expected output"""
-    clean_data("PT")
+    data_raw = load_data()
+    data_cleaned = clean_data(data_raw, "PT")
+    save_data(data_cleaned)
+    
     pt_life_expectancy_actual = pd.read_csv(OUTPUT_DIR / "pt_life_expectancy.csv")
     pd.testing.assert_frame_equal(
         pt_life_expectancy_actual, pt_life_expectancy_expected
