40d2a56bbc6ca4c3dc4f17998480895e3e8ced29
Goncalo-Chambel <goncalorrc@gmail.com>
2024-05-12 11:36:26 +0100

    Unit tests complete

--- life_expectancy/load_save.py
+++ life_expectancy/load_save.py
@@ -1,10 +1,11 @@
 import pandas as pd
+from . import DATA_DIR
 
 def load_data(filename, sep="\t") -> pd.DataFrame:
     """Function to load data from file"""
     data = pd.read_csv(filename, sep=sep)
     return data
 
-def save_data(data: pd.DataFrame) -> None:
+def save_data(data: pd.DataFrame, filename = DATA_DIR / "pt_life_expectancy.csv") -> None:
     """Function to save data to file"""
-    data.to_csv("life_expectancy/data/pt_life_expectancy.csv", index=False)
+    data.to_csv(filename, index=False)
--- life_expectancy/tests/test_load_save.py
+++ life_expectancy/tests/test_load_save.py
@@ -1,9 +1,15 @@
 import pandas as pd
 from life_expectancy.load_save import load_data, save_data
-from . import FIXTURES_DIR
+from unittest.mock import patch
+from . import FIXTURES_DIR, DATA_DIR
 
 def test_save_data():
-    pass
+    with patch('pandas.DataFrame.to_csv') as mock_to_csv:
+
+        df = pd.DataFrame({'a': [1, 2, 3], 'b': [4, 5, 6]})
+        print(df)
+        save_data(df, "dummy_data.csv")
+        mock_to_csv.assert_called_once_with("dummy_data.csv", index=False)
 
 def test_load_data(eu_life_expectancy_raw_sample):
     data = load_data(FIXTURES_DIR / "eu_life_expectancy_raw_sample.tsv")
